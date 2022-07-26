7dd3ed80a45386c275e8a6380fbc7522290eface
“poornae” <poornae_9611@hotmail.com>
2022-07-26 21:54:10 +0530

    next commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,105 @@
-import React, { useEffect, useState } from "react";
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu } from "antd";
+import React, { useState } from "react";
+import { render } from "react-dom";
+import "./App.css";
+import "antd/dist/antd.css";
 
-function App() {
-  const [backendData, setBackendData] = useState([{}]);
+const { Header, Content, Footer, Sider } = Layout;
 
-  useEffect(() => {
-    fetch("/api")
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-  }, []);
-
-  return <div></div>;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+
+const items = [
+  getItem("Dashboard", "1", <PieChartOutlined />),
+  getItem("Clients", "2", <DesktopOutlined />),
+  getItem("Reservations", "3", <DesktopOutlined />),
+  getItem("Calendar", "4", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <FileOutlined />),
+];
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />
+        <Content
+          style={{
+            margin: "0 16px",
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Salon Express ©2022 Created by Eyepax
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default App;
